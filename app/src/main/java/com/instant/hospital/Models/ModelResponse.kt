a9685b7a58c05f3b7d2975265ab5f7d9c1095775
Hossam <hossamegyqandel@gmail.com>
2022-04-25 09:07:17 +0200

    Fix navController Rebuild docNurseCaseDetails Rebuild docSelectNurse Rebuild calls ModelResponse Added

---
+++ app/src/main/java/com/instant/hospital/Models/ModelResponse.kt
@@ -0,0 +1,6 @@
+package com.instant.hospital.Models
+
+data class ModelResponse(
+    val message: String,
+    val status: Int
+)
---
+++ app/src/main/java/com/instant/hospital/Ui/Fragments/Common/DoctorCaseViewModel.kt
@@ -0,0 +1,38 @@
+package com.instant.hospital.Ui.Fragments.Common
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.instant.hospital.Data.Network.NetworkState
+import com.instant.hospital.Data.Network.SingleLiveEvent
+import com.instant.hospital.Data.Network.WebServices
+import com.instant.hospital.Utils.Const
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class DoctorCaseViewModel @Inject constructor(private val webServices: WebServices) : ViewModel() {
+
+
+    private val _doctorCaseLiveData = SingleLiveEvent<NetworkState>()
+    val doctorCaseLiveData get() = _doctorCaseLiveData
+
+    fun getCaseDetailsById(caseId: Int = 7){
+        viewModelScope.launch(Dispatchers.IO){
+            try {
+                val objectDataReceiver = webServices.getSingleCaseDetailsByCaseId(caseId)
+
+                    if (objectDataReceiver.status == Const.BACKEND_STATUS_CODE_SUCCESS){
+                        _doctorCaseLiveData.postValue(NetworkState.getLoaded(objectDataReceiver))
+                }
+                else {
+                        _doctorCaseLiveData.postValue(NetworkState.getErrorMessage(objectDataReceiver.message))
+                    }
+            }catch (ex: Exception){
+                _doctorCaseLiveData.postValue(NetworkState.getExeptionErrorMessage(ex))
+            }
+        }
+    }
+
+}
